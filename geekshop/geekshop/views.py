3aae12a47f4a6e8bd699d168b9bb609fbcb6ba8c
tortilla <tortilla1310@gmail.com>
2022-08-26 19:14:30 +0200

    Lesson 3 is done

---
+++ geekshop/geekshop/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from basketapp.models import Basket
+from mainapp.models import Product
+
+
+def index(request):
+    title = 'GeekShop'
+    products = Product.objects.all()[:4]
+
+    context = {
+        'title': title,
+        'products': products,
+    }
+    return render(request, 'geekshop/index.html', context)
+
+
+def contacts(request):
+    title = 'контакты'
+    context = {
+        'title': title,
+    }
+    return render(request, 'geekshop/contacts.html', context)
